ff92a018fcafefc00d35d9cd0e56954e07147aa0
gcruzgar <g.cruz@leeds.ac.uk>
2019-01-22 14:18:56 +0000

    data preprocessing

--- external_migration.py
+++ external_migration.py
@@ -7,4 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd 
 
-odm = pd.read_csv("data/interantional_migration_2011.csv", skiprows=10, skipfooter=5) # load migration data
+odm = pd.read_csv("data/international_migration_2011.csv", skiprows=8, skipfooter=6) # load migration data
+odm = odm.rename(columns = {                                   # rename column for ease
+    "currently residing in : 2011 census merged local authority district": "destination"})
+odm = odm.set_index("destination")                             # set destination as index
+odm.columns.name = "origin"                                    # columns are the origin
+odm.head()
