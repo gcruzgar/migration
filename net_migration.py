742dfcea71cc9cc1d7e03784d788d07914e09c04
gcruzgar <g.cruz@leeds.ac.uk>
2019-01-22 14:10:01 +0000

    script description

--- net_migration.py
+++ net_migration.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-
+This script reads census OD data to output the net migration in each local authority district.
+A shapefile of the UK is read to plot a cloropleth map of net migration.
 """
 import pandas as pd 
 import numpy as np
